fe2f511276854e6e836d1428e068178f1a7e7e88
Michael Farmer <mfarm19@gmail.com>
2023-03-22 14:15:38 -0400

    Add project files.

---
+++ SFTP Console App/Program.cs
@@ -0,0 +1,280 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Renci.SshNet.Sftp;
+using System.IO;
+using System.Text;
+
+class Program
+{
+	public static List<SftpFile> retryList = new List<SftpFile>();
+	public static StringBuilder log = new StringBuilder();
+
+	// Declare SFTP servers
+	public static SftpClient client1 = new SftpClient("127.0.0.1", "username", "password");
+	public static SftpClient client2 = new SftpClient("127.0.0.1", "username", "password");
+
+	// TODO: either hardcode the actual location into the code or give the user a prompt to enter it. We are using OpenSSH Serve in dev, but the actual path should be different and my have a different format in production
+	public static string clientOneDirectoryLocation = "/C:/Users/username.DESKTOP-6FQE1ER/FTPTests/";
+	public static string clientTwoDirectoryLocation = "/C:/Users/username.DESKTOP-6FQE1ER/FTPTests/";
+
+	static void Main(string[] args)
+	{
+		while (true)
+		{
+			try
+			{
+				// Connect to first SFTP server
+				client1 = connectToSFTPServer("SFTP Server 1");
+				client1.Connect();
+				Console.WriteLine("Connected to SFTP Server 1.");
+				log.Append($"Connected to SFTP Server 1.{Environment.NewLine}");
+				break;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.Message}");
+				log.Append($"Error: {ex.Message}");
+			}
+		}
+
+		while (true)
+		{
+			try
+			{
+				// Connect to second SFTP server
+				client2 = connectToSFTPServer("SFTP Server 2");
+				client2.Connect();
+				Console.WriteLine("Connected to SFTP Server 2.");
+				log.Append($"Connected to SFTP Server 2.{Environment.NewLine}");
+				break;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.Message}");
+				log.Append($"Error: {ex.Message}");
+			}
+		}
+
+		// Attempt to get all files from the given directory 
+		IEnumerable<SftpFile> allFileList = client1.ListDirectory(clientOneDirectoryLocation);
+
+		// Locate failedToProcessFiles.txt file if it exists
+		string recoveredFileListPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "failedToProcessFiles.txt");
+		string[] recoveredFileList = File.Exists(recoveredFileListPath) ? File.ReadAllLines(recoveredFileListPath) : Array.Empty<string>();
+
+		ConsoleKeyInfo reprocessResponse = new ConsoleKeyInfo();
+		if (recoveredFileList.Count() > 0)
+		{
+			// Wait for next keystroked
+			Console.Write($"There are {recoveredFileList.Count()} files that were not processed successfully. Try to process them again? (y/n) ");
+			reprocessResponse = Console.ReadKey();
+
+			while (reprocessResponse.KeyChar.ToString().ToLower() != "n" && reprocessResponse.KeyChar.ToString().ToLower() != "y")
+			{
+				Console.Write($"{Environment.NewLine}There are {recoveredFileList.Count()} files that were not processed successfully. Try to process them again? (y/n) ");
+				reprocessResponse = Console.ReadKey();
+			}
+
+			if (reprocessResponse.KeyChar.ToString().ToLower() == "n")
+			{
+				Console.WriteLine($"{Environment.NewLine}Recoverd files were not processed.");
+			}
+			else
+			{
+				// Add a line break, it looks better this way
+				Console.WriteLine("");
+
+				// Attempt to locate the recovered files
+				IEnumerable<SftpFile> fileList = allFileList.Where(file => recoveredFileList.Contains(file.Name)).ToList();
+
+				// Attempt to process all recovered files
+				processFiles(fileList);
+
+				// Delete the failedToProcessFiles file
+				File.Delete(recoveredFileListPath);
+			}
+		}
+
+		if (recoveredFileList.Count() == 0 || reprocessResponse.KeyChar.ToString().ToLower() == "n")
+		{
+			// Look for files with a specific extension, we're using txt files as a placeholder
+			// TODO: update with real file extension
+			IEnumerable<SftpFile> txtFiles = allFileList.Where(file => file.Name.EndsWith(".txt"));
+
+			// Wait for next keystroked
+			Console.Write($"There are {txtFiles.Count()} files in the given directory. Process all? (y/n) ");
+			ConsoleKeyInfo value = Console.ReadKey();
+
+			while (value.KeyChar.ToString().ToLower() != "n" && value.KeyChar.ToString().ToLower() != "y")
+			{
+				Console.Write($"{Environment.NewLine}There are {txtFiles.Count()} files in the given directory. Process all? (y/n) ");
+				value = Console.ReadKey();
+			}
+
+			if (value.KeyChar.ToString().ToLower() == "n")
+			{
+				Console.WriteLine($"{Environment.NewLine}Task canceled. {Environment.NewLine}Press any key to exit...");
+				Console.ReadKey();
+				Environment.Exit(0);
+				return;
+			}
+
+			// Add a line break, it looks better this way
+			Console.WriteLine("");
+
+			// Attempt to process all files
+			processFiles(txtFiles);
+		}
+
+		// Attempt to process files that failed to process
+		while (retryList.Count() > 0)
+		{
+			// Ask if the user want's to try and process failed files
+			Console.Write($"{retryList.Count()} files were unable to be processed. Retry? (y/n) ");
+			ConsoleKeyInfo refuseRetry = Console.ReadKey();
+
+			while (refuseRetry.KeyChar.ToString().ToLower() != "n" && refuseRetry.KeyChar.ToString().ToLower() != "y")
+			{
+				Console.Write($"{Environment.NewLine}{retryList.Count()} files were unable to be processed. Retry? (y/n) ");
+				refuseRetry = Console.ReadKey();
+			}
+
+			Console.WriteLine("");
+
+			if (refuseRetry.KeyChar.ToString().ToLower() == "n")
+			{
+				// Save failed files to txt file
+				File.WriteAllText(recoveredFileListPath, string.Join("\n", retryList.Select(item => item.Name)));
+
+				// Log the failed files and exit the while loop
+				log.Append($"{retryList.Count()} files were not processed and were saved to \"failedToProcessFiles.txt\".");
+				Console.WriteLine($"{retryList.Count()} files were not processed and were saved to \"failedToProcessFiles.txt\".");
+				break;
+			}
+
+			IEnumerable<SftpFile> retryListAsEnumerable = retryList.AsEnumerable();
+			processFiles(retryListAsEnumerable);
+		}
+
+		// Disconnect from servers
+		client1.Disconnect();
+		client2.Disconnect();
+
+		// Create the logs directory if it doesn't exist
+		string logsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+		if (!Directory.Exists(logsDirectory)) Directory.CreateDirectory(logsDirectory);
+
+		// Write the log data to disk
+		File.WriteAllText(Path.Combine(logsDirectory, $"log-{DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss")}.txt"), log.ToString());
+
+		Console.WriteLine($"Task Completed. {Environment.NewLine}Press any key to exit...");
+		Console.ReadKey();
+		Environment.Exit(0);
+	}
+
+	public static SftpClient connectToSFTPServer(string client)
+	{
+		// Skip this step if we're debugging
+		if (System.Diagnostics.Debugger.IsAttached)
+		{
+			return new SftpClient("127.0.0.1", "username", "password");
+		}
+
+		// Wait for next keystroked
+		Console.Write($"What is the server address for {client}? ");
+		string address = Console.ReadLine();
+		Console.Write("Username: ");
+		string username = Console.ReadLine();
+		Console.Write("Password: ");
+		string password = hideKeyup();
+
+		return new SftpClient(address, username, password);
+	}
+
+	public static string hideKeyup()
+	{
+		string password = "";
+
+		while (true)
+		{
+			ConsoleKeyInfo key = Console.ReadKey(true);
+			if (key.Key == ConsoleKey.Enter)
+			{
+				Console.WriteLine("");
+				break; // Exit the loop when the user presses Enter
+			}
+			else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+			{
+				password = password.Substring(0, password.Length - 1);
+				Console.Write("\b \b"); // Erase the last character from the console
+			}
+			else if (char.IsLetterOrDigit(key.KeyChar))
+			{
+				password += key.KeyChar;
+				Console.Write("*"); // Print a * to the console instead of the actual character
+			}
+		}
+
+		return password;
+	}
+
+	public static void processFiles(IEnumerable<SftpFile> fileList)
+	{
+		// Empty the retry list
+		retryList = new List<SftpFile>();
+
+		foreach (SftpFile file in fileList)
+		{
+			try
+			{
+				Console.WriteLine($"Processing {file.Name}");
+
+				// Attempt to transform the file
+				MemoryStream mergedStream = transformFile(file);
+
+				// Attempt to upload the file to server two
+				client2.UploadFile(mergedStream, clientTwoDirectoryLocation + file.Name);
+
+				log.Append($"Processed: {file.Name}. {Environment.NewLine}");
+			}
+			catch (Exception ex)
+			{
+				retryList.Add(file);
+				Console.WriteLine($"Error: {ex.Message} | Unable to process {file.Name}.");
+				log.Append($"Unable to process {file.Name}.{Environment.NewLine}Error: {ex.Message}");
+			}
+		}
+	}
+
+	public static MemoryStream transformFile(SftpFile file)
+	{
+		MemoryStream mergedStream = new MemoryStream();
+
+		// TODO: rework this as it may not be as easy as this depending on the file format we are given.
+		try
+		{
+			// Read file from first server
+			MemoryStream fileStream = new MemoryStream();
+			client1.DownloadFile(clientOneDirectoryLocation + file.Name, fileStream);
+
+			// Modify file, we are just adding "xxx" to the start and end of the txt file here because we don't know how we actually to modify the file
+			var text = "xxx" + System.Text.Encoding.UTF8.GetString(fileStream.ToArray()) + "xxx";
+
+			// Merge with new file
+			var mergedText = text + "Hello World";
+
+			// Write merged file to second server
+			mergedStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(mergedText));
+		}
+		catch (Exception ex)
+		{
+			retryList.Add(file);
+			Console.WriteLine($"Error: {ex.Message} | Unable to process {file.Name}.");
+			log.Append($"Unable to process {file.Name}.{Environment.NewLine}Error: {ex.Message}");
+		}
+
+		return mergedStream;
+	}
+}
